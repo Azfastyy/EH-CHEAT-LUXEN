7fd52432f33c3eef6e1da98f2f059808875367a9
Azfastyy <azfasto0@gmail.com>
2025-10-21 12:53:50 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1399,7 +1399,7 @@ local function safeCarTeleport(targetPosition)
     
     local startPos = primaryPart.Position
     local distance = (targetPosition - startPos).Magnitude
-    local speed = 300
+    local speed = 500
     local speedInStuds = speed * 0.277778
     local duration = distance / speedInStuds
     
